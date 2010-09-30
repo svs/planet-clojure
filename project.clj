63a40d08d5acf40dba24c957a895d3b6184032ce
Siddharth <svs@intellecap.net>
2010-10-01 03:14:44 +0530

    first commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject planet "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]
+		 [compojure "0.4.1"]
+		 [ring/ring-jetty-adapter "0.2.3"]
+		 [ring/ring-devel "0.2.3"]
+		 [enlive "1.0.0-SNAPSHOT"]
+		 [prolefeed "0.1-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :main planet
+)
---
+++ test/planet/test/core.clj
@@ -0,0 +1,6 @@
+(ns planet.test.core
+  (:use [planet.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
